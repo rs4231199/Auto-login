c70051de1384fb4e2b2e56c7d206d26384f08d8c
Rahul <rs4231199@gmail.com>
2020-04-07 18:57:44 +0530

    firefox-addon-first-commit

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 // enable if you want to login everytime browser is opended.
-// chrome.runtime.onStartup.addListener(function() {
-//     chrome.tabs.create({ "url": "https://gateway.iitmandi.ac.in/facstaff/login.php" });
+// browser.runtime.onStartup.addListener(function() {
+//     browser.tabs.create({ "url": "https://gateway.iitmandi.ac.in/facstaff/login.php" });
 // });
 
-chrome.browserAction.onClicked.addListener(function(activeTab)
+browser.browserAction.onClicked.addListener(function(activeTab)
 {
-    chrome.tabs.create({ "url": "https://gateway.iitmandi.ac.in/facstaff/login.php" });
+    browser.tabs.create({ "url": "https://gateway.iitmandi.ac.in/facstaff/login.php" });
 });
 
 const echo = (command) => {
 	console.log("Command : ", command);
 	if (command === "login") { 
-		chrome.tabs.create({ "url": "https://gateway.iitmandi.ac.in/facstaff/login.php" });
+		browser.tabs.create({ "url": "https://gateway.iitmandi.ac.in/facstaff/login.php" });
     }
 }
 
-chrome.commands.onCommand.addListener(echo);
+browser.commands.onCommand.addListener(echo);
